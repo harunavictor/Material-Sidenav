220bc939e4c59b529cb3296516341e5ff2e7907f
Haruna Victor <harunavictor754@gmail.com>
2020-04-12 17:24:23 +0100

    sidenav manipulation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,39 @@
+import { CreateActivityComponent } from './create-activity/create-activity/create-activity.component';
+import { CreateAccountComponent } from './create-account/create-account/create-account.component';
+import { AccountsComponent } from './accounts/accounts/accounts.component';
+import { CreateContactComponent } from './create-contact/create-contact/create-contact.component';
+import { ContactsComponent } from './contacts/contacts/contacts.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:  '', redirectTo:  'contacts', pathMatch:  'full' },
+  {
+    path: 'accounts',
+    component: AccountsComponent
+  },
+  {
+    path: 'create-account',
+    component: CreateAccountComponent
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent
+  },
+  {
+    path: 'create-contact',
+    component: CreateContactComponent
+  },
+  {
+    path: 'activities',
+    component: CreateActivityComponent
+  },
+  {
+    path: 'create-activity',
+    component: CreateActivityComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './material-module';
+import { SideNavbarComponent } from './side-navbar/side-navbar.component';
+import { AccountsComponent } from './accounts/accounts/accounts.component';
+import { CreateAccountComponent } from './create-account/create-account/create-account.component';
+import { ContactsComponent } from './contacts/contacts/contacts.component';
+import { CreateContactComponent } from './create-contact/create-contact/create-contact.component';
+import { ActivitiesComponent } from './activities/activities/activities.component';
+import { CreateActivityComponent } from './create-activity/create-activity/create-activity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavbarComponent,
+    AccountsComponent,
+    CreateAccountComponent,
+    ContactsComponent,
+    CreateContactComponent,
+    ActivitiesComponent,
+    CreateActivityComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
